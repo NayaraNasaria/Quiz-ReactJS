21022c290b9395cdba74d55bc2f8af99d3789e12
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2023-03-15 04:58:14 -0300

    Quiz em React JS

---
+++ src/data/questions.js
@@ -0,0 +1,91 @@
+const data = [
+  {
+    question: "O que é Vanilla JavaScript?",
+    options: [
+      "JavaScript puro",
+      "Uma biblioteca JavaScript",
+      "Um framework JavaScript",
+      "Um compilador de JavaScript",
+    ],
+    answer: "JavaScript puro",
+  },
+  {
+    question: "Com qual instrução declaramos uma constante em JavaScript?",
+    options: ["const", "let", "var", "define"],
+    answer: "const",
+  },
+  {
+    question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+    options: ["string", "number", "boolean", "float"],
+    answer: "float",
+  },
+  {
+    question: "Qual dos métodos a seguir seleciona um elemento?",
+    options: ["querySelector", "parseInt", "sort", "reduce"],
+    answer: "querySelector",
+  },
+  {
+    question:
+      "Qual destas propriedades da a quantidade de elementos de um array?",
+    options: ["qty", "length", "items", "index"],
+    answer: "length",
+  },
+  {
+    question: "Qual valor é exibido pelo seguinte comando:\nprint(int(53.785))",
+    options: ["Nada, é produzido um erro de execução", "54", "53.785", "53"],
+    answer: "53",
+  },
+  {
+    question: "Qual é o valor a expressão a seguir?\n16 - 2 * 5 // 3 + 1",
+    options: ["13", "24", "14", "16"],
+    answer: "14",
+  },
+  {
+    question: "HTML é:",
+    options: [
+      "Linguaguem de Programação",
+      "Linguaguem de Marcação",
+      "Pseudocódigo",
+      "Linguaguem",
+    ],
+    answer: "Linguaguem de Marcação",
+  },
+  {
+    question: "Qual é a sintaxe correta para gerar 'Hello World' em Java?",
+    options: [
+      'Console.WriteLine("Hello World")',
+      'print("Hello World")',
+      'echo("Hello World")',
+      'System.out.println("Hello World")',
+    ],
+    answer: 'System.out.println("Hello World")',
+  },
+  {
+    question: "O que significa PHP?",
+    options: [
+      "Personal Hypertext Processor",
+      "Private Home Page",
+      "Programing Hypertext Page",
+      "Personal Hypertext Preprocessor",
+    ],
+    answer: "Personal Hypertext Preprocessor",
+  },
+  {
+    question:
+      "Qual instrução SQL é usada para extrair dados de um banco de dados?",
+    options: ["OPEN", "GET", "SELECT", "EXTRACT"],
+    answer: "SELECT",
+  },
+  {
+    question: "Qual é o comando correto para criar um novo projeto React?",
+    options: [
+      "npx create-react-app",
+      "npm create-react-app myReactApp",
+      "npm create-react-app",
+      "npx create-react-app myReactApp",
+    ],
+    answer: "npx create-react-app myReactApp",
+  },
+];
+
+export default data;
